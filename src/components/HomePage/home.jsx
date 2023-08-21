42cb7b7e16e4eb53855469d54bfe2d8b12edf12c
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-08-21 22:58:12 +0530

    changes for deployment

--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -6,7 +6,7 @@ const home = ()=>{
       <div>
        <h1>Contacto App</h1>
        <div>
-        <img src="../../../images/contact.png" alt="" height="300" width ="300"/>
+        <img src="https://img.freepik.com/free-vector/phone_53876-25489.jpg" alt="" height="400" width ="400"/>
        </div>
        <button onClick={()=>{
         navigate("/login")
